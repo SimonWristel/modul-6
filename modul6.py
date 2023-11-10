2938001c44e28eb3874df8e68c89009b77edc1f7
Simonwristel <Simon.wristel@elev.ga.ntig.se>
2023-11-10 09:49:45 +0100

    Modul 6

---
+++ modul6.py
@@ -0,0 +1,29 @@
+import os
+os.system('cls')
+
+#1
+def add(x, y):
+    return y + x
+
+print(add(3, 3))
+
+print("Skriv in ditt tal ")
+
+#2
+def getname(name):
+    return name
+
+print(getname("Simon"))
+
+#3
+
+def add(x, y):
+    return x + y
+for i in range(10):
+    print(add(20,10))
+
+#4
+my_list=["Simon", "Victor", "Valter"]
+
+print(my_list, "Vill du ta bort, lägga till, ändra?")
+
